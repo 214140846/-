573c8c3fef276937dd62c11c6b538f73f6cb5b10
apple <888888>
2020-05-20 00:46:59 +0800

    颜色分类

---
+++ src/com/solution/leetcode/yansefenlei.java
@@ -0,0 +1,57 @@
+package com.solution.leetcode;
+
+public class yansefenlei {
+    /*
+    75. 颜色分类
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+注意:
+不能使用代码库中的排序函数来解决这道题。
+
+示例:
+
+输入: [2,0,2,1,1,0]
+输出: [0,0,1,1,2,2]
+进阶：
+
+一个直观的解决方案是使用计数排序的两趟扫描算法。
+首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+你能想出一个仅使用常数空间的一趟扫描算法吗？
+    * */
+}
+class Solution4 {
+    public void sortColors(int[] nums) {
+        if(nums.length<2)return ;
+        //三路快排
+        int left=0;//找0
+        int right=nums.length-1;//找2
+        int head=0;//找1
+
+        while(head<=right){
+            //head找到0的时候说明head和left互换
+            if(nums[head]==0){
+                swap(nums,head,left);
+                left++;
+                head++;
+            }
+            //head找到1的时候不用换 走就行 因为是中位数
+            else if(nums[head]==1){
+                head++;
+            }
+            else if(nums[head]==2){
+                swap(nums,head,right);
+
+                right--;
+            }
+
+        }
+    }
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+}
